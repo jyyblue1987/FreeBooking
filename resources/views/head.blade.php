8a5a31465861e4c6b2e015d4e94f40a1cb098ee6
Kashif Hussain <Kashif Hussain>
2016-04-08 13:01:59 +0500

    newly added files included files

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -4,8 +4,8 @@
 <title>Project Flyer</title>
 
 
-<link rel="stylesheet" href="{{ URL::asset('/') }}css/app.css">
 <link rel="stylesheet" href="{{ URL::asset('/') }}css/fonts/awesome-fonts.css">
-<link rel="stylesheet" href="{{ URL::asset('/') }}css/custom/custom.min.css">
+<link rel="stylesheet" href="{{ URL::asset('/') }}css/app.css">
+<link rel="stylesheet" href="{{ URL::asset('/') }}css/custom/custom.css">
 <link rel="stylesheet" href="{{ URL::asset('/') }}css/libs/libs.css">
 <link href="{{ URL::asset('/') }}css/libs/blueimp-gallery.min.css" rel="stylesheet">
--- resources/views/scripts/headScripts.blade.php
+++ resources/views/scripts/headScripts.blade.php
@@ -5,6 +5,11 @@
 <script src="{{ URL::asset('/') }}js/flow/ng-flow-standalone.min.js"></script>
 <script src="{{ URL::asset('/') }}js/spinner/angular-spinners.js"></script>
 <script src="{{ URL::asset('/') }}js/plugins/app.js"></script>
+
+<script src="{{ URL::asset('/') }}js/angularJs/controllers/appControllers.js"></script>
+<script src="{{ URL::asset('/') }}js/angularJs/controllers/hotelControllers.js"></script>
+<script src="{{ URL::asset('/') }}js/angularJs/controllers/roomControllers.js"></script>
+
+<script src="{{ URL::asset('/') }}js/angularJs/directives/appDirectives.js"></script>
+
 <script src="{{ URL::asset('/') }}js/services/server.js"></script>
-
-
