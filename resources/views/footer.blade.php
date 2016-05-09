ff62e2c12f1f153d6d20479d233ef065e33880c3
Kashif Hussain <Kashif Hussain>
2016-05-09 11:11:15 +0500

    footer text modified

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,4 @@
 <footer id="site-foot" class="site-foot clearfix">
-    <p class="left">&copy; Copyright 2014 <strong>APPBOARD</strong>, All rights reserved.</p>
+    <p class="left">&copy; Copyright 2005 - 2016 <a href="http://www.freebookings.nl"><strong>FREEBOOKINGS.NL</strong></a>, All rights reserved.</p>
     <p class="right">v1.1</p>
 </footer>
