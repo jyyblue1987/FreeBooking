7b921450f44eae5ce7660eb522023709d9f448be
Kashif Hussain <Kashif Hussain>
2016-05-15 15:11:24 +0500

    arrangements form creatd

--- resources/views/admin/partials/arrangements/arrangement.blade.php
+++ resources/views/admin/partials/arrangements/arrangement.blade.php
@@ -1,1 +1,134 @@
-Arragement layout will go here
+
+<div class="page page-form-wizard clearfix ng-scope">
+
+    <ol class="breadcrumb breadcrumb-small">
+        <li>Your Hotel</li>
+        <li class="active"><a href="#/hotels"> Add New Arrangements</a></li>
+    </ol>
+
+    <div class="page-wrap panel panel-body">
+
+		<div ng-controller="ArrangementController">
+
+			<form  name="arrangementsForm" class="form-horizontal col-lg-10" ng-submit="arrangementsForm.$valid && addNewArrangement()">
+				<div class="form-block">
+					
+					<h3 class="text-uppercase text-bold mb30">Add New Arrangements</h3>
+					
+				    <div class="form-group">
+				        <label class="col-lg-2 control-label">Name</label>
+				        <div class="col-lg-4">
+				            <input type="text" name="name" class="form-control" placeholder="Arrangement Name" required>
+				        </div>
+				    </div>
+				    <div class="form-group">
+				        <label class="col-lg-2 control-label">Transcription</label>
+				        <div class="col-lg-10">
+				            <div class="text-angular-editor" text-angular></div>
+				        </div>
+				    </div>
+				    <div class="form-group">
+				        <label class="col-lg-2 control-label">Special</label>
+				        <div class="col-lg-4">
+							<div class="ui-toggle ui-toggle-lg ui-toggle-info">
+								<label class="ui-toggle-inline">
+									<input type="checkbox" name="arrangementSpecial"> 
+									<span></span>
+								</label>
+							</div>
+							<!-- #end ui-toggle -->
+				        </div>
+				    </div>
+				    <div class="form-group">
+				        <label class="col-lg-2 control-label">Maximum number</label>
+				        <div class="col-lg-4">
+				        	<div class="select-box">
+				        		<select name="arrangementMax" class="form-control">
+				        			<option value="">Select</option>
+				        			<option value="1">1</option>
+				        			<option value="2">2</option>
+				        			<option value="3">3</option>
+				        			<option value="4">4</option>
+				        			<option value="5">5</option>
+				        		</select>	
+				        	</div><!-- /select-box -->
+				        </div>
+				    </div>
+				    <div class="form-group">
+				        <label class="col-lg-2 control-label">Arrangement for several days</label>
+				        <div class="col-lg-10">
+							<div class="ui-toggle ui-toggle-lg ui-toggle-info">
+								<label class="ui-toggle-inline">
+									<input type="checkbox" ng-model="arrangementsForm.days" name="arrangementDays"> 
+									<span></span>
+								</label>
+							</div><!-- #end ui-toggle -->
+							<div ng-show="arrangementsForm.days">
+								this arrangement consists of
+								<div class="select-box inline">
+					        		<select name="arrangementMax" class="form-control">
+					        			<option value="1">1</option>
+					        			<option value="2">2</option>
+					        			<option value="3">3</option>
+					        			<option value="4">4</option>
+					        			<option value="5">5</option>
+					        		</select>
+								</div><!-- /select-box -->
+								nights
+							</div>
+				        </div>
+				    </div>
+				    <div class="form-group">
+				        <label class="col-lg-2 control-label">Rebate System</label>
+				        <div class="col-lg-10">
+							<div class="ui-toggle ui-toggle-lg ui-toggle-info">
+								<label class="ui-toggle-inline">
+									<input type="checkbox" ng-model="arrangementsForm.rebate" name="rebate"> 
+									<span></span>
+								</label>
+							</div><!-- #end ui-toggle -->
+							<div ng-show="arrangementsForm.rebate">
+								3=2, Book 3 nights = Pay for 2 nights (multiple 3 nights)
+							</div>
+				        </div>
+				    </div>
+				    <div class="form-group">
+				        <label class="col-lg-2 control-label">Type</label>
+				        <div class="col-lg-4">
+			        		<select name="arrangementType" class="form-control" multiple>
+								<option value="" selected="selected"></option>
+								<option value="voorjaar">spring</option>
+								<option value="bosrijk">bosrijk</option>
+								<option value="culinaire">culinary</option>
+								<option value="feestelijk">festive</option>
+								<option value="gezondheid">health</option>
+								<option value="kindvriendelijk">child-friendly</option>
+								<option value="natuur_en_sport">nature and sports</option>
+								<option value="romantisch">romantic</option>
+								<option value="stedelijk_en_cultuur">city and culture</option>
+								<option value="verwen">relax</option>
+								<option value="weekend">weekend</option>
+								<option value="zakelijk">business..</option>
+								<option value="zomer">summer</option>
+								<option value="Park_Sleep_Fly">Park Sleep Fly</option>
+								<option value="Aan_het_water">Aan het water</option>
+			        		</select>	
+				        </div>
+				        <div class="col-lg-6">
+				        	<p class="help-block">You can select max. 3 types, press the CTRL-key and select the desired type</p>
+				        	<p class="help-block">If you do not find your type arrangement, please inform us <a href="mailto:info@freebookings.nl">freebookings.nl</a></p>
+				        </div>
+				    </div>
+
+				</div><!-- /form-block -->
+				<div class="form-block">
+					<div class="alert-info"><p>Please enter the number of nights, when the arrangement consists of several nights. The price below is the total price.</p></div>
+					<div class="alert-info"><p>The price is per night, if you do not wish an arrangement for several days (please enter no price per person). A surcharge is always per room, per night!</p></div>
+				</div><!-- /form-block -->
+			</form><!-- /arrangementsForm -->
+
+		</div><!-- /ArrangementController -->
+
+    </div>
+
+</div>
