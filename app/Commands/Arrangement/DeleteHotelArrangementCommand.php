6aa6e6a103de7493d2aa4f7d6ff3e7558d2da8f0
wowthemes <support@wow-themes.com>
2016-05-31 17:14:27 +0500

    Command to delete arrangement of a specific hotel

---
+++ app/Commands/Arrangement/DeleteHotelArrangementCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Commands\Arrangement;
+
+use App\Commands\Command;
+use Illuminate\Contracts\Bus\SelfHandling;
+
+class DeleteHotelArrangementCommand extends Command implements SelfHandling
+{
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
