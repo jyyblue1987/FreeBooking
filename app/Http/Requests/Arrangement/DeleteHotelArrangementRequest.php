1c1cafe8d27911edf3f82fdd70d23b0ae7b09817
wowthemes <support@wow-themes.com>
2016-05-31 17:24:52 +0500

    Request the validate the arrangement delete URL

--- app/Http/Requests/Arrangement/DeleteHotelArrangementRequest.php
+++ app/Http/Requests/Arrangement/DeleteHotelArrangementRequest.php
@@ -13,7 +13,7 @@ class DeleteHotelArrangementRequest extends Request
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class DeleteHotelArrangementRequest extends Request
     public function rules()
     {
         return [
-            //
+            'hotel_id' => 'required|exists:hotels,id',
+            'arrangement_id' => 'required|exists:arrangements,id'
         ];
     }
 }
