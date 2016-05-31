a9896c693fbb09cef5d161676840d6332f2da639
wowthemes <support@wow-themes.com>
2016-05-31 17:20:58 +0500

    Command to show single arrangement of a specific hotel

--- app/Commands/Arrangement/ShowHotelArrangementCommand.php
+++ app/Commands/Arrangement/ShowHotelArrangementCommand.php
@@ -4,17 +4,45 @@ namespace App\Commands\Arrangement;
 
 use App\Commands\Command;
 use Illuminate\Contracts\Bus\SelfHandling;
+use App\Freebooking\Repositories\Hotel\HotelRepository;
+use App\Freebooking\Repositories\Arrangement\HotelArrangementRepository;
+use App\Freebooking\Repositories\Arrangement\HotelArrangementDescriptionRepository;
+use App\Freebooking\Exceptions\Hotel\HotelNotFound;
+use App\Freebooking\Exceptions\Hotel\HotelNotBelongToUser;
+use App\Arrangement;
+use App\ArrangementDescription;
+use Illuminate\Foundation\Application;
 
 class ShowHotelArrangementCommand extends Command implements SelfHandling
 {
+
+    /**
+     * @var
+     */
+    private $user_id;
+
+    /**
+     * @var
+     */
+    private $hotel_id;
+
+    /**
+     * @var
+     */
+    private $arrangement_id;
+
     /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct( $user_id, $hotel_id, $arrangement_id )
     {
-        //
+        $this->user_id = $user_id;
+
+        $this->hotel_id = $hotel_id;
+
+        $this->arrangement_id = $arrangement_id;
     }
 
     /**
@@ -22,8 +50,25 @@ class ShowHotelArrangementCommand extends Command implements SelfHandling
      *
      * @return void
      */
-    public function handle()
+    public function handle(HotelRepository $hotelRepository, HotelArrangementRepository $hotelArrangementRepository)
     {
-        //
+        $hotel = $hotelRepository->getHotelByUserId($this->user_id);
+        if ( ! $hotel) {
+            throw new HotelNotFound();
+        }
+
+        if($hotel->id != $this->hotel_id)
+        {
+            throw new HotelNotBelongToUser();
+        }
+
+        //dd($this->arrangement_id);
+        $arrangement = new Arrangement();
+
+        $data = (object)$arrangement->find($this->arrangement_id)->toArray();
+        //dd((object)$data);
+        $data->arrangementDescription = '';
+
+        return $data;
     }
 }
