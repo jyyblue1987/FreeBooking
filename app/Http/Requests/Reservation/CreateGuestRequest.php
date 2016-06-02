1576f4e13ac255728f6858db8333c0c32c43acd7
wowthemes <support@wow-themes.com>
2016-06-03 01:52:34 +0500

    Request to validate data for creating guest

---
+++ app/Http/Requests/Reservation/CreateGuestRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Reservation;
+
+use App\Http\Requests\Request;
+
+class CreateGuestRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'hotel_id' => 'required|exists:hotels,id',
+            'gender' => 'required',
+            'name' => 'required',
+            'address' => 'required',
+            'state' => 'required',
+            'city' => 'required',
+            'zip' => 'required|integer',
+            'country' => 'required',
+            'phone' => 'required',
+            'fax' => 'required',
+            'email' => 'required|email',
+            'language' => 'required',
+        ];
+    }
+}
