0ca393bdd633ea5038580193a41ac11c8b1c458a
wowthemes <support@wow-themes.com>
2016-06-04 23:40:53 +0500

    Command to show a single guest

--- app/Commands/Reservation/ShowGuestCommand.php
+++ app/Commands/Reservation/ShowGuestCommand.php
@@ -3,18 +3,26 @@
 namespace App\Commands\Reservation;
 
 use App\Commands\Command;
+use App\Freebooking\Repositories\Reservation\GuestsRepository;
+use App\Guests;
+use Doctrine\Instantiator\Exception\InvalidArgumentException;
 use Illuminate\Contracts\Bus\SelfHandling;
+
 
 class ShowGuestCommand extends Command implements SelfHandling
 {
+    /**
+     * @var
+     */
+    protected  $id;
     /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($id)
     {
-        //
+        $this->id = $id;
     }
 
     /**
@@ -22,8 +30,18 @@ class ShowGuestCommand extends Command implements SelfHandling
      *
      * @return void
      */
-    public function handle()
+    public function handle(GuestsRepository $guestsRepository)
     {
-        //
+
+        if( ! is_numeric($this->id) ) {
+            throw new InvalidArgumentException();
+        }
+
+        $guest = new Guests();
+
+        $data = $guestsRepository->getGuest( $guest, $this->id );
+
+        return $data;
+
     }
 }
