d3c2f6f95676ca3c59395461f352320408978b6e
wowthemes <support@wow-themes.com>
2016-06-04 23:41:35 +0500

    Request handler to show single guest

--- app/Http/Requests/Reservation/ShowGuestRequest.php
+++ app/Http/Requests/Reservation/ShowGuestRequest.php
@@ -13,7 +13,7 @@ class ShowGuestRequest extends Request
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class ShowGuestRequest extends Request
     public function rules()
     {
         return [
-            //
+            //'guests'    => 'required'
         ];
     }
 }
