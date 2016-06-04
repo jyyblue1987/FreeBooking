2ef62af2e9a872f997dbf031dbd0637c43d16da1
wowthemes <support@wow-themes.com>
2016-06-04 23:40:17 +0500

    Command to destroy the guest

--- app/Commands/Reservation/DestroyGuestCommand.php
+++ app/Commands/Reservation/DestroyGuestCommand.php
@@ -3,27 +3,45 @@
 namespace App\Commands\Reservation;
 
 use App\Commands\Command;
+use App\Freebooking\Repositories\Reservation\GuestsRepository;
+use App\Guests;
+use Doctrine\Instantiator\Exception\InvalidArgumentException;
 use Illuminate\Contracts\Bus\SelfHandling;
 
 class DestroyGuestCommand extends Command implements SelfHandling
 {
     /**
+     * @var
+     */
+    protected $id;
+
+    /**
      * Create a new command instance.
      *
-     * @return void
+     * @param $id
      */
-    public function __construct()
+    public function __construct($id)
     {
-        //
+        $this->id = $id;
     }
 
     /**
      * Execute the command.
      *
-     * @return void
+     * @param GuestsRepository $guestsRepository
+     * @return int
+     * @throws InvalidArgumentException
      */
-    public function handle()
+    public function handle(GuestsRepository $guestsRepository)
     {
-        //
+        if( ! is_numeric($this->id) ) {
+            throw new InvalidArgumentException();
+        }
+
+        $guest = new Guests();
+
+        $data = $guestsRepository->deleteGuest( $guest, $this->id );
+
+        return $data;
     }
 }
