77fda245526855dc6f52a1d273d8f43a79348038
wowthemes <support@wow-themes.com>
2016-06-05 01:58:42 +0500

    Create reservaton command file

--- app/Commands/Reservation/CreateReservationCommand.php
+++ app/Commands/Reservation/CreateReservationCommand.php
@@ -3,18 +3,76 @@
 namespace App\Commands\Reservation;
 
 use App\Commands\Command;
+use App\Freebooking\Repositories\Reservation\ReservationRepository;
+use App\Reservation;
 use Illuminate\Contracts\Bus\SelfHandling;
 
 class CreateReservationCommand extends Command implements SelfHandling
 {
     /**
+     * @var
+     */
+    protected $guest_id;
+
+    /**
+     * @var
+     */
+    protected $hotel_id;
+
+    /**
+     * @var
+     */
+    protected $room_id;
+
+    /**
+     * @var
+     */
+    protected $checkin;
+
+    /**
+     * @var
+     */
+    protected $checkout;
+
+    /**
+     * @var
+     */
+    protected $arrangement_id;
+
+    /**
+     * @var
+     */
+    protected $num_of_rooms;
+
+    /**
+     * @var
+     */
+    protected $num_of_persons;
+
+    /**
+     * @var
+     */
+    protected $arrival_time;
+
+
+    /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct( $guest_id, $hotel_id, $room_id, $checkin, $checkout, $arrangement_id, $num_of_rooms, $num_of_persons, $arrival_time
+)
     {
-        //
+        $this->guest_id         = $guest_id;
+        $this->hotel_id         = $hotel_id;
+        $this->room_id          = $room_id;
+        $this->checkin          = $checkin;
+        $this->checkout         = $checkout;
+        $this->arrangement_id   = $arrangement_id;
+        $this->num_of_rooms     = $num_of_rooms;
+        $this->num_of_persons   = $num_of_persons;
+        $this->arrival_time     = $arrival_time;
+
     }
 
     /**
@@ -22,8 +80,21 @@ class CreateReservationCommand extends Command implements SelfHandling
      *
      * @return void
      */
-    public function handle()
+    public function handle( ReservationRepository $reservationRepository )
     {
-        //
+        // @todo check whether guest is already reserved with this hotel
+
+        $fields = ['guest_id', 'hotel_id', 'room_id', 'checkin', 'checkout', 'arrangement_id', 'num_of_rooms', 'num_of_persons', 'arrival_time' ];
+
+        $reservation = new Reservation();
+
+        foreach ($fields as $field )
+        {
+            $reservation->$field = $this->$field;
+        }
+
+        $reservationRepository->create($reservation);
+
+        return $reservation;
     }
 }
