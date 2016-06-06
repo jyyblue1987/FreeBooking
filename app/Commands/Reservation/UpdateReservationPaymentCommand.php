b6b48c05175f00126560aa939207d8ea5f1ce392
Shehzad hussain <Shehzad hussain>
2016-06-06 18:25:57 +0500

    Update reservation payment command

--- app/Commands/Reservation/UpdateReservationPaymentCommand.php
+++ app/Commands/Reservation/UpdateReservationPaymentCommand.php
@@ -3,18 +3,66 @@
 namespace App\Commands\Reservation;
 
 use App\Commands\Command;
+use App\Freebooking\Repositories\Reservation\ReservationPaymentRepository;
+use App\ReservationPayment;
 use Illuminate\Contracts\Bus\SelfHandling;
 
 class UpdateReservationPaymentCommand extends Command implements SelfHandling
 {
     /**
+     * @var
+     */
+    private $id;
+
+    /**
+     * @var
+     */
+    private $reservation_id;
+
+    /**
+     * @var
+     */
+    private $option;
+
+    /**
+     * @var
+     */
+    private $cc_name;
+
+    /**
+     * @var
+     */
+    private $cc_num;
+
+    /**
+     * @var
+     */
+    private $cc_type;
+
+    /**
+     * @var
+     */
+    private $cvv;
+    /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct( $id, $reservation_id, $option, $cc_name, $cc_num, $cc_type, $cvv )
     {
-        //
+        $this->id = $id;
+
+        $this->reservation_id = $reservation_id;
+
+        $this->option = $option;
+
+        $this->cc_name = $cc_name;
+
+        $this->cc_num = $cc_num;
+
+        $this->cc_type = $cc_type;
+
+        $this->cvv = $cvv;
     }
 
     /**
@@ -22,8 +70,19 @@ class UpdateReservationPaymentCommand extends Command implements SelfHandling
      *
      * @return void
      */
-    public function handle()
+    public function handle( ReservationPaymentRepository $reservationPaymentRepository )
     {
-        //
+        $fields = ['id', 'reservation_id', 'option', 'cc_name', 'cc_num', 'cc_type', 'cvv' ];
+
+        $reservation = new ReservationPayment();
+
+        foreach ($fields as $field )
+        {
+            $reservation->$field = $this->$field;
+        }
+
+        $reservationPaymentRepository->update( $reservation, $this->id );
+
+        return $reservation;
     }
 }
