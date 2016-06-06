d80973c7832907592de62c26831c614f6934456f
Shehzad hussain <Shehzad hussain>
2016-06-06 18:33:29 +0500

    reservation payment transformer

--- app/Freebooking/Transformers/Reservation/ReservationPaymentTransformer.php
+++ app/Freebooking/Transformers/Reservation/ReservationPaymentTransformer.php
@@ -5,7 +5,7 @@ namespace App\Freebooking\Transformers\Reservation;
 use App\Freebooking\Transformers\Transformer;
 
 
-class ReservationTransformer extends Transformer
+class ReservationPaymentTransformer extends Transformer
 {
     /**
      * ReservationTransformer constructor.
@@ -23,15 +23,13 @@ class ReservationTransformer extends Transformer
     {
 
         return [
-            'guest_id'          => $reservation->guest_id,
-            'hotel_id'          => $reservation->hotel_id,
-            'room_id'           => $reservation->room_id,
-            'checkin'           => $reservation->checkin,
-            'checkout'          => $reservation->checkout,
-            'arrangement_id'    => $reservation->arrangement_id,
-            'num_of_rooms'      => $reservation->num_of_rooms,
-            'num_of_persons'    => $reservation->num_of_persons,
-            'arrival_time'      => $reservation->arrival_time,
+            'id'                      => $reservation->id,
+            'reservation_id'          => $reservation->reservation_id,
+            'option'                  => $reservation->option,
+            'cc_name'                 => $reservation->cc_name,
+            'cc_num'                  => $reservation->cc_num,
+            'cc_type'                 => $reservation->cc_type,
+            'cvv'                     => $reservation->cvv
 
         ];
 
