e91f269530bf28643fac1510fcf1213fff2d7a7b
Kashif Hussain <Kashif Hussain>
2016-06-19 00:18:58 +0500

    blacklist memebers

---
+++ resources/views/admin/partials/bilk/bilk.blade.php
@@ -0,0 +1,110 @@
+
+<div class="page page-form-wizard clearfix ng-scope">
+
+    <ol class="breadcrumb breadcrumb-small">
+        <li>Your Hotel</li>
+        <li class="active"><a href="#/hotels"> View Blacklist</a></li>
+    </ol>
+
+    <div class="page-wrap">
+
+		<div class="panel panel-lined mb30">
+			<div class="panel-heading">Search for a person on the Blacklist</div>
+			<div class="panel-body">
+				<div class="row">
+					<form  name="bilkForm" class="col-md-8 col-md-push-1 form-horizontal" ng-submit="bilkForm.$valid && searchBilk()">
+						<div class="form-group">
+							<label class="col-md-2 control-label text-uppercase small text-bold">Name:</label>
+							<div class="col-md-8">
+								<input type="text" placeholder="Name" class="form-control">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-2 control-label text-uppercase small text-bold">Description:</label>
+							<div class="col-md-8">
+								<input type="text" placeholder="Description" class="form-control">
+							</div>
+						</div>
+						<div class="form-group mb10">		
+							<div class="col-md-8 col-md-push-2">
+								<button class="btn btn-primary" type="submit">Show</button>
+								<button class="btn btn-info" type="button">Show All</button>
+							</div>
+						</div>
+						<div class="form-group mb0">		
+							<div class="col-md-8 col-md-push-2">
+								<a href="#/addblacklist" class="btn btn-danger">Put a person on the black list</a>
+							</div>
+						</div>
+
+					</form>
+				</div>
+			</div><!-- /panel-body -->
+		</div><!-- /panel -->
+		<div class="panel panel-lined mb30 table-responsive">
+			<div class="panel-heading">Blacklist</div>
+			<div class="panel-body">
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Description</th>
+							<th>Since</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><a href="#">gast njs adam van musschenbroekstraat 183 2522am den haag 0644135174 nadjaadam1512@live.nl</a></td>
+							<td><p>Proberen te boeken op basis van '' eenmalig incasso'' Als dit niet mogelijk is, is de pinpas er niet of is deze geblokkeerd bij het hotel! 5 jaar verkering om iets te vieren is ook een reden van boeking.. Neem deze gasten niet aan of neem direct contact op met de politie. geboekt in Crown in, Eindhoven en Landgoed Groot Warnsborn.</p></td>
+							<td>30-08-2011</td>
+						</tr>
+						<tr>
+							<td><a href="#">E.W. Bak Van Ostadestraat 6 Oud Beijerland edwin.bak@kpnplanet.nl</a></td>
+							<td><p>No show. Wel geboekt, niet betaald, niet gekomen.</p></td>
+							<td>21-06-2010</td>
+						</tr>
+						<tr>
+							<td><a href="#">Sander Houtbraken</a></td>
+							<td>
+								<p>Gebruikt verschillende andere achternamen, maar wel altijd de voornaam Sander. Heeft zich ingeschreven met papieren van een vriend die een bedrijf had waar de rekening heen moest (schade � 4.500,00). Pas op: hij is heel charmant en vooral heel erg behulpzaam</p>
+								<p>Valt op omdat hij vreselijk dik is. Zie ook opgelicht.n</p>
+								<p>Heeft al veel collegae gedupeerd en wordt op dit moment gezocht</p>
+							</td>
+							<td>25-03-2010</td>
+						</tr>
+						<tr>
+							<td><a href="#">cees paauwe einderkoolhof 11 puth schingen</a></td>
+							<td>fietser, boekt en is heel joviaal, blijft langer laat wat troep op de kamer staan en komt niet meer. Man van eind 50.</td>
+							<td>29-07-2009</td>
+						</tr>
+						<tr>
+							<td><a href="#">Dijk, W Bilderdijkhof 21 3221VE Hellevoetsluis</a></td>
+							<td>
+								<p>Geslacht: Man</p>
+								<p>boekt kamer en zegt weer af wegens ongeluk????</p>
+							</td>
+							<td>05-06-2009</td>
+						</tr>
+						<tr>
+							<td><a href="#">Jong, E.G. Melissantstraat 4 2729CV Zoetermeer</a></td>
+							<td><p>Geslacht: Man</p></td>
+							<td>17-05-2007</td>
+						</tr>
+						<tr>
+							<td><a href="#">marc Monsieurs ganzerikhof 30 5044 bt tilburg</a></td>
+							<td>
+								<p>Geslacht: Man</p>
+								<p>Man komt alleen met een zielig verhaal over zijn pinpas die doormidden is en andere smoesjes.</p>
+								<p>Er zijn in omgeving Tilburg al 2 hotels gedupeerd.</p>
+							</td>
+							<td>26-04-2007</td>
+						</tr>
+					</tbody>
+				</table>
+			</div><!-- /panel-body -->
+		</div><!-- /panel -->
+
+
+    </div>
+
+</div>
