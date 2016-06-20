b834e96671a9cd17ff101000dcca771e3fcafbf7
wowthemes <support@wow-themes.com>
2016-06-20 18:36:57 +0500

    Update php documentor

--- app/Freebooking/Repositories/Arrangement/HotelArrangementDescriptionRepository.php
+++ app/Freebooking/Repositories/Arrangement/HotelArrangementDescriptionRepository.php
@@ -5,14 +5,29 @@ namespace App\Freebooking\Repositories\Arrangement;
 use Illuminate\Support\Facades\DB;
 use App\ArrangementDescription;
 
+/**
+ * Class HotelArrangementDescriptionRepository
+ * @package App\Freebooking\Repositories\Arrangement
+ */
+
 class HotelArrangementDescriptionRepository
 {
 
+	/**
+	 * @param ArrangementDescription $arrangementDescription
+	 * @return bool
+	 */
     public function create(ArrangementDescription $arrangementDescription)
     {
         return $arrangementDescription->save();
     }
 
+	/**
+	 * @param $arragementId
+	 * @param $hotelId
+	 * @param $language
+	 * @return mixed
+	 */
     public function getByArrangementIdAndHotelIdAndLanguage($arragementId, $hotelId, $language)
     {
 
