807a1a885abb52035dc313ad3f76b183d4170c60
wowthemes <support@wow-themes.com>
2016-06-20 18:37:48 +0500

    Update php documentor

--- app/Freebooking/Repositories/Reservation/ReservationRepository.php
+++ app/Freebooking/Repositories/Reservation/ReservationRepository.php
@@ -6,9 +6,18 @@ use App\Hotel;
 use App\Reservation;
 use Illuminate\Support\Facades\DB;
 
+
+/**
+ * Class ReservationRepository
+ * @package App\Freebooking\Repositories\Reservation
+ */
 class ReservationRepository
 {
 
+	/**
+	 * @param Reservation $reservation
+	 * @return array
+	 */
     public function getAll( Reservation $reservation )
     {
         $data = $reservation->all()->toArray();
@@ -16,16 +25,30 @@ class ReservationRepository
         return $data;
     }
 
+	/**
+	 * @param Reservation $reservation
+	 * @return bool
+	 */
     public function create( Reservation $reservation )
     {
         return $reservation->save();
     }
 
+	/**
+	 * @param Reservation $reservation
+	 * @param $id
+	 * @return mixed
+	 */
     public function update( Reservation $reservation, $id )
     {
         return $reservation->where( 'id', $id )->update( $reservation->getAttributes() );
     }
 
+	/**
+	 * @param Reservation $reservation
+	 * @param $id
+	 * @return int
+	 */
     public function delete( Reservation $reservation, $id )
     {
         $data = $reservation->destroy($id);
@@ -33,6 +56,11 @@ class ReservationRepository
         return $data;
     }
 
+    /**
+     * @param Reservation $reservation
+     * @param $id
+     * @return mixed
+     */
     public function getReservation( Reservation $reservation, $id )
     {
         return $reservation->where( 'id', $id )->first();
